dc9a19b045c66eef22ed62b2596d633b32eb314a
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-01-13 02:37:58 +0700

    create and style meetupForm component

---
+++ components/meetups/MeetupForm.js
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import classes from "./MeetupForm.module.css";
+
+const MeetupForm = (props) => {
+    const titleInputRef = useRef();
+    const imgInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("onSubmitHandler");
+    const title = titleInputRef.current.value;
+    const img = imgInputRef.current.value;
+    const address = addressInputRef.current.value;
+    const description = descriptionInputRef.current.value;
+
+    if(!title || !img || !address || !description) {
+        console.log("Invalid form input");
+        return;
+    }
+
+    console.log(`title = ${title}; img = ${img}; address = ${address}; description = ${description}`);
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler} className={classes.form}>
+      <div className={classes.inputWrapper}>
+        <label htmlFor="title">Meetup Title</label>
+        <input type="text" id="title" ref={titleInputRef} />
+      </div>
+      <div className={classes.inputWrapper}>
+        <label htmlFor="img">Meetup Image</label>
+        <input type="text" id="img" ref={imgInputRef} />
+      </div>
+      <div className={classes.inputWrapper}>
+        <label htmlFor="address">Address</label>
+        <input type="text" id="address" ref={addressInputRef} />
+      </div>
+      <div className={classes.inputWrapper}>
+        <label htmlFor="description">Description</label>
+        <textarea ref={descriptionInputRef}></textarea>
+      </div>
+      <div className={classes.btnWrapper}>
+        <button>Add Meetup</button>
+      </div>
+    </form>
+  );
+};
+
+export default MeetupForm;
--- pages/new-meetups.js
+++ pages/new-meetups.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
+import MeetupForm from '../components/meetups/MeetupForm';
 
 const NewMeetup = (props) => {
     return (
         <Layout>
-            <h1>Add New Meetup Page</h1>
+            <MeetupForm />
         </Layout>
     );
 };
